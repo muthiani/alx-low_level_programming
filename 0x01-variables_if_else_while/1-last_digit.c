5dd579794f3e090664ea6f7c51cfa02078ea1517
Vaati M <sidomvaha@gmail.com>
2023-02-20 10:32:56 -0800

    Compares last digit of number n to 5,6 and 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	lastDigit = n % 10;
 	if (lastDigit < 6 && lastDigit != 0)
-		printf("Last digit of %d is %d and is less that 6 and not 0\n", n,
+		printf("Last digit of %d is %d andis less that 6 and not 0\n", n,
 				lastDigit);
 	else if (lastDigit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
