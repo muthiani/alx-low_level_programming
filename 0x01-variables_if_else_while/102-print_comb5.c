39bc814448c706c91a359912341c851ee9d494f9
Vaati M <sidomvaha@gmail.com>
2023-02-21 01:21:56 -0800

    prints all possible combinations of two two-digit numbers with putchar

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,8 +11,8 @@
 int main(void)
 {
 	int i, j, k, l;
-
-    	for (i = 0; i < 10; i++)
+	
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -20,7 +20,7 @@ int main(void)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					if (k + l > i + j)
+					if ((k * 10) + l > (i * 10) + j)
 					{
 						putchar('0' + i);
 						putchar('0' + j);
@@ -34,7 +34,7 @@ int main(void)
 			}
 		}
 	}
-    putchar('\n');
-
-    return (0);
+	putchar('\n');
+	
+	return (0);
 }
