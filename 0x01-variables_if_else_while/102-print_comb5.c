2fdc1a8b36b37e761dc3c271fe2cc80a7e1c94b0
Vaati M <sidomvaha@gmail.com>
2023-02-21 01:05:16 -0800

    prints all possible combinations of two two-digit numbers with putchar

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints combinations of two two-digit numbers
+ *
+ * Return : Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+    	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				for (l = 0; l < 10; l++)
+				{
+					if (k + l > i + j)
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(' ');
+						putchar('0' + k);
+						putchar('0' + l);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+    putchar('\n');
+
+    return (0);
+}
