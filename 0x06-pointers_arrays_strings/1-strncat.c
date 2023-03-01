40a34e7efd9c7bfdc8cc49125c144a3501ca0d26
Vaati M <sidomvaha@gmail.com>
2023-03-01 08:46:14 -0800

    Add n bytes of string b to string a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,15 @@
-/**
- * _strncat - a function that concatenates two strings.
- *
- * @dest: pointer to destination input
- * @src: pointer to source input
- * @n: most number of bytes from @src
- *
- * Return: @dest
-*/
+#include "main.h"
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j;
+        int i = 0, j;
 
-	/*find size of dest array*/
-	while (dest[i])
-		i++;
+        while (dest[i])
+                i++;
 
-	/**
-	 * src does not need to be null terminated
-	 * if it contains n or more bytes
-	*/
-	for (j = 0; j < n && src[j] != '\0'; j++)
-		dest[i + j] = src[j];
-	/*null terminate dest*/
-	dest[i + j] = '\0';
+        for (j = 0; j < n && src[j] != '\0' ; j++)
+                dest[i + j] = src[j];
+        dest[i+j] = '\0';
 
-	return (dest);
+        return (dest);
 }
