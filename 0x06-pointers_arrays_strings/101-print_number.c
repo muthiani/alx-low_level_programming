a0f28c8eb433ec2f57e44932de7a71869122066e
Vaati M <sidomvaha@gmail.com>
2023-03-07 18:29:08 -0800

    Print numbers with putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-#define ROT13IN  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-#define ROT13OUT "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
-
 /**
  * print_number - using only _putchar
  * @n: the integer to print
@@ -11,20 +8,14 @@
  */
 void print_number(int n)
 {
-	int i, p;
-
 	if (n < 0)
-		_putchar('-');
-	for (p = 0, i = 1000000000; i > 0; i /= 10)
 	{
-		int d = (n / i) % 10;
-
-		d = d < 0 ? -d : d;
-		if (d || p || i == 1)
-		{
-			_putchar(d + '0');
-			p++;
-		}
-
+		_putchar('-');
+		n = -n;
 	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar(n % 10 + 48);
 }
